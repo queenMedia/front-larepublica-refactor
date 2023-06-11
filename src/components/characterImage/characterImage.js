ba7d36c57dcd560ad366c1f82c317fc8588cc594
ronyderra <derrarony@gmail.com>
2023-06-11 15:08:43 +0300

    style

--- src/components/characterImage/characterImage.js
+++ src/components/characterImage/characterImage.js
@@ -1,11 +1,13 @@
-import "./characterImage.css"
+import "./characterImage.css";
 
 export const CharacterImage = (props) => {
   return (
     <>
       <div class="img-with-text">
         <img src={props.src} alt="sometext" />
-        <p>{props.txt}</p>
+        <p>
+          <strong>{props.txt}</strong>
+        </p>
       </div>
     </>
   );
