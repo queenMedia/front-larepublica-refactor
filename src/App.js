bbc147985aa9d6d9e3bb64468b15971821227cca
ronyderra <derrarony@gmail.com>
2023-06-11 13:33:19 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import "./App.css";
-import { Header } from "./components/Header/Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className="container">
+      <header className="headerContainer">
+        <div className="topHeader">
+          <div>menu</div>
+          <img src="https://www.lps.martinviz.com/_app/immutable/assets/logo_lr_white-cb374dfd.svg " />
+          <div>search</div>
+        </div>
+        <div className="bottomHeader">
+          <div>ÚLTIMAS NOTICIAS</div>
+          <div>POLÍTICA</div>
+          <div>ECONOMÍA</div>
+          <div>SOCIEDAD</div>
+          <div>MUNDO</div>
+          <div>DEPORTES</div>
+          <div>ESPECTÁCULOS</div>
+          <div>LaRepublica</div>
+          <div>CINE Y SERIES</div>
+          <div>DÓLAR</div>
+          <div>SUSCRÍBETE</div>
+        </div>
+      </header>
+      <body className="bodyContainer">
+        <div className="content">
+          <div>title</div>
+          <div>image</div>
+          <div>image</div>
+          <div>image</div>
+          <div>image</div>
+          <div>image</div>
+          <div>image</div>
+          <div>image</div>
+        </div>
+        <div className="sidebar">
+          <div>title</div>
+          <div>image</div>
+          <div>image</div>
+          <div>image</div>
+          <div>image</div>
+          <div>image</div>
+          <div>image</div>
+          <div>image</div>
+        </div>
+      </body>
+      <footer className="footer">
+        <div>footer</div>
+      </footer>
     </div>
   );
 }
