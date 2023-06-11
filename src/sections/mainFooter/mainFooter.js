c5711a3a81447757892300a13aab4a081a6b5f5f
ronyderra <derrarony@gmail.com>
2023-06-11 19:49:07 +0300

    footer

---
+++ src/sections/mainFooter/mainFooter.js
@@ -0,0 +1,41 @@
+import "./mainFooter.css";
+const MainFooter = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <ul className="footer-table">
+          <li className="footer-table-item">Ultimas Noticias</li>
+          <li className="footer-table-item">Política</li>
+          <li className="footer-table-item">Economía</li>
+          <li className="footer-table-item">Sociedad</li>
+          <li className="footer-table-item">Deportes</li>
+          <li className="footer-table-item">Espectáculos</li>
+          <li className="footer-table-item">Cine & Series</li>
+          <li className="footer-table-item">Mundo</li>
+          <li className="footer-table-item">Tendencias</li>
+          <li className="footer-table-item">Tecnología</li>
+          <li className="footer-table-item">Cultura Asiática</li>
+          <li className="footer-table-item">Loterías y sorteos</li>
+          <li className="footer-table-item">Datos LR</li>
+          <li className="footer-table-item">Columnistas</li>
+          <li className="footer-table-item">Verificador</li>
+          <li className="footer-table-item">Argentina</li>
+          <li className="footer-table-item">México</li>
+          <li className="footer-table-item">Venezuela</li>
+          <li className="footer-table-item">Venezuela</li>
+        </ul>
+
+        <div className="footer-rightSide">
+          <span></span>
+          <div className="footer-rightSide-container">
+            <img src="https://www.lps.martinviz.com/_app/immutable/assets/logo-red-larepublica-ec0aae3e.svg" />
+            <p>©Todos los derechos reservados - 2023</p>
+            <p>Políticas y estándares - Términos de Uso</p>
+            <p>Contáctenos</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default MainFooter;
