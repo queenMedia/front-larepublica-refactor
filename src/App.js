74d42c982c0af74abb83313f4da7893ce2a8a603
ronyderra <derrarony@gmail.com>
2023-06-11 16:16:15 +0300

    txt

--- src/App.js
+++ src/App.js
@@ -186,7 +186,340 @@ function App() {
             </p>
           </div>
           <ReadMore onClick={setReadmore} />
-          {readMore ? <div>more</div> : ""}
+          {readMore ? (
+            <>
+              <div className="content-text">
+                <h2>
+                  EL LUCRATIVO SECRETO PARA GENERAR DINERO QUE LOS GRANDES
+                  BANCOS NO QUIEREN QUE SEPAS
+                </h2>
+                <p>Martin Vizcarra continúa,</p>
+                <p>
+                  "Nuestra economía está pasando por momentos difíciles y esta
+                  es la solución que la gente ha estado esperando. Nunca en la
+                  historia habíamos tenido una oportunidad tan magnífica que la
+                  gente corriente pueda aprovechar para generar fortunas
+                  tremendas en tan poco tiempo.
+                </p>
+                <p>
+                  Algunas personas tienen dudas sobre si probarlo o no porque es
+                  algo muy diferente. ¡Y por eso es que los grandes bancos están
+                  tratando de ocultarlo! Los grandes bancos están todo el tiempo
+                  generando propaganda y diciendo que las criptomonedas y las
+                  plataformas como Bitcoin Code son un fraude. ¿Por qué? Temen
+                  que sus beneficios corporativos disminuyan una vez que sus
+                  clientes aprendan a generar riquezas masivas por su cuenta.
+                </p>
+
+                <p>
+                  La verdad es que las criptomonedas son la revolución de
+                  nuestra era y cualquier persona que no se una a esta
+                  oportunidad se pierde de mucho. Ya he recibido llamadas
+                  agresivas y amenazantes por parte de grandes corporaciones
+                  financieras porque estoy poniendo esta tecnología a la vista
+                  de todos. Pero a mí eso no me importa. La gente en América
+                  Latina ya está empezando a conocer la verdad y solo es
+                  cuestión de tiempo para que cada vez más personas sepan sobre
+                  esto.
+                </p>
+                <p>
+                  Lo comparto porque ya he recibido cientos de correos de
+                  personas que me agradecen por compartir este secreto. Mi
+                  favorito es el de un joven que gracias al dinero que hizo con
+                  Bitcoin Code le regaló a su hermano menor el auto de sus
+                  sueños: un Ferrari 488 Pista. Esta plataforma de verdad está
+                  haciendo que la vida de todas las personas del mundo sea un
+                  poco mejor".
+                </p>
+              </div>
+              <CharacterImage
+                src={
+                  "https://pgenpro-test.s3.amazonaws.com/characters/martinvizcarra-img6.jpg"
+                }
+                txt={
+                  "Desde que mi hermano menor era un niño, quiso un Ferrari 488 Pista por su clásico y hermoso color rojo y su poderoso motor. Bien, últimamente he estado haciendo enormes cantidades de dinero gracias a una nueva plataforma para hacer transacciones con criptomonedas (escríbanme si quieren más detalles), ¡Y lo sorprendí con el AUTO DE SUS SUEÑOS! ¡Su raección NO TIENE PRECIO! ¡Te amo, hermanito!"
+                }
+              />
+              <div className="content-text">
+                <h2>
+                  ¿BITCOIN CODE DE VERDAD FUNCIONA? LO PROBAMOS NOSOTROS MISMOS
+                </h2>
+                <p>
+                  Nuestros editores en jefe no nos dejaron publicar la
+                  entrevista con Martin Vizcarra hasta verificar que Bitcoin
+                  Code es un método legítimo para hacer dinero desde casa. La
+                  gerencia no quería que publicáramos ningún tipo de información
+                  que pudiera causar que los ciudadanos de América Latina
+                  perdieran el dinero que ganan con su esfuerzo.
+                </p>
+                <p>
+                  Así que nuestro equipo editorial probó Bitcoin Code para
+                  asegurarse que en verdad funciona como dice Martin Vizcarra.
+                  Uno de nuestros editores en línea, Juan Gómez, se ofreció como
+                  voluntario y arriesgó su propio dinero para probar Bitcoin
+                  Code.
+                </p>
+                <p>
+                  Juan es un hombre de 37 años, padre de dos hijos. Su esposa se
+                  enfermó el año pasado y por eso perdió su trabajo. Juan
+                  admitió que tenía problemas financieros y que esta oportunidad
+                  de inversión podría ser la solución.
+                </p>
+              </div>
+              <CharacterImage
+                src={
+                  "https://pgenpro-test.s3.amazonaws.com/characters/martinvizcarra-img7.jpg"
+                }
+                txt={
+                  "Su familia estaba luchando para que el dinero les alcanzara y tenían sus esperanzas puestas en que Bitcoin Code les ayudaría a aliviar la presión, así que Juan decidió probar el sistema y hablar sobre los resultados que obtuvo."
+                }
+              />
+              <div className="content-text">
+                <h6>Juan informa que:</h6>
+                <p>
+                  "Al principio, cuando escuché la entrevista de Martin
+                  Vizcarra, pensé que era un chiste. Hacer dinero desde casa no
+                  es más que un sueño. Decidí que igual lo iba a probar, dadas
+                  mis circunstancias económicas. Además, todo sea por el
+                  periodismo de calidad.
+                </p>
+                <p>
+                  Vi un video introductorio acerca de la plataforma y después me
+                  registré. Me pareció que el video era demasiado prometedor,
+                  pero decidí dejar de lado mi escepticismo. Un par de horas
+                  después recibí la llamada del que sería mi inversionista
+                  personal. Respondió todas mis preguntas y dudas, y me aseguró
+                  que haría dinero. Punto.
+                </p>
+                <p>
+                  Mi inversionista personal incluso me prometió que, si perdía
+                  un solo centavo, me reembolsaría mis S/1200 de inmediato. Así
+                  de seguro estaba él de que esto me iba a cambiar la vida. Les
+                  digo, eso sí es un servicio al cliente, es mucho mejor que
+                  cualquier otro que haya visto en mi vida y no me sorprende que
+                  los bancos estén tan preocupados.
+                </p>
+                <p>
+                  Una vez que recibí acceso a la plataforma, deposité mi
+                  inversión inicial de S/1200. La plataforma hace transacciones
+                  en euros (€) y dólares ($) pero puedes retirar los fondos en
+                  la moneda que prefieras.
+                </p>
+                <p>
+                  El sistema de Bitcoin Code es una plataforma que hace
+                  transacciones automáticas con criptomonedas. El software
+                  utiliza algoritmos avanzados de inteligencia artificial y
+                  aprendizaje automático para predecir exactamente cuándo las
+                  criptomonedas subirán o bajarán. Luego compra y vende por ti,
+                  de forma automática y a toda hora. La tecnología ha mejorado
+                  nuestras vidas de todas las formas imaginables, ¿por qué no
+                  usarla también para hacer más dinero?"
+                </p>
+                <h2>LOS RESULTADOS REALES DE JUAN CON EL SISTEMA</h2>
+                <p>
+                  "Después de una hora de haber depositado los S/1200, el
+                  software empezó a hacer transacciones por mí. Para ser
+                  honesto, me ponía nervioso perder todo mi dinero. Y
+                  efectivamente, ¡la primera transacción resultó en una pérdida
+                  de S/89!
+                </p>
+                <p>
+                  Sentí un nudo en la garganta. Pensé que me habían estafado. Ya
+                  me estaba preparando para llamar a mi inversionista personal y
+                  pedirle que me regresara mi dinero, pero luego recordé algo
+                  que me había dicho antes: El algoritmo hace predicciones
+                  correctas el 80-89% del tiempo. No vas a ganar TODAS las
+                  transacciones, pero vas a ganar suficiente y al final te
+                  generará ganancias
+                </p>
+                <p>
+                  Así que dejé que el software siguiera haciendo transacciones
+                  por mí mientras lo observaba sin perderme un segundo. ¡La
+                  siguiente transacción fue positiva! Solo me dejó S/68, pero
+                  algo es algo. Luego de eso hice S/182. Después de eso S/78, lo
+                  que para ese momento era una ganancia total de S/240. ¡Y todo
+                  pasó en menos de 5 minutos!
+                </p>
+                <p>
+                  Luego de eso empecé a acumular dinero como si nada, no podía
+                  creer lo que veían mis ojos.
+                </p>
+                <strong>
+                  Cada vez que refrescaba la pantalla, mis ganancias crecían y
+                  crecían. Me sentía como si estuviera alucinando con drogas,
+                  todo era demasiado emocionante
+                </strong>
+                <p>
+                  Ahora sé por qué Martin Vizcarra está siempre de buen humor. Y
+                  por qué los grandes bancos no quieren que la gente sepa nada
+                  sobre esta laguna. Al final del día había hecho más de S/2.700
+                  en ganancias, ¡nada mal para haber empezado con S/1200! Estaba
+                  tan emocionado que apenas pude dormir.
+                </p>
+                <p>
+                  El día siguiente era martes y tenía que ir a trabajar otra
+                  vez. Si les soy sincero (y no le digan esto a mi jefe), fue
+                  difícil concentrarme en el trabajo mientras pensaba que el
+                  software de Bitcoin Code me estaba generando dinero.
+                </p>
+                <p>
+                  Me escapé al baño varias veces para revisar cómo iban mis
+                  ganancias, que seguían creciendo (con pérdidas pequeñas aquí y
+                  allá). En la noche, después de que mis hijos se quedaron
+                  dormidos, mi cuenta tenía S/4,834.¡Más de lo que gano en una
+                  SEMANA de trabajo!
+                </p>
+                <p>
+                  Cuando se terminó la semana ya había hecho S/19,168. Retiré
+                  exactamente S/15.000 y reinvertí el resto. En dos días recibí
+                  mi cheque en el correo por S/15.000 exactos. ¡No podía creer
+                  que estaba despierto!"
+                </p>
+              </div>
+              <CharacterImage
+                src={
+                  "https://pgenpro-test.s3.amazonaws.com/characters/martinvizcarra-img8.jpg"
+                }
+                txt={
+                  "Juan recibió un cheque por S/15.000 por sus primeras dos semanas en la plataforma Bitcoin Code."
+                }
+              />
+              <div className="content-text">
+                <h6>Juan dice,</h6>
+                <p>
+                  “Ahora hago regularmente unos S/2,500 – 5.000 al día gracias a
+                  Bitcoin Code. El dinero se deposita en mi cuenta del banco
+                  cada par de días. Con solo unos cuantos clics recibo mi dinero
+                  en las siguientes 24 a 48 horas. Cada vez que veo la
+                  transferencia en mi cuenta, tengo que pellizcarme para
+                  asegurarme de que no estoy soñando.
+                </p>
+                <p>
+                  Por suerte, me ENCANTA mi trabajo porque aquí puedo contar
+                  historias importantes (como esta) a las personas. De lo
+                  contrario, ya habría renunciado. Sin embargo, ya planeé unas
+                  vacaciones con mi familia a Bali, Indonesia, para celebrar que
+                  pagamos todas las deudas y que al fin nuestras finanzas están
+                  en orden.
+                </p>
+                <p>
+                  Nada de esto sería posible sin la generosidad del Sr. Martin
+                  Vizcarra, quien compartió este secreto en la TV en vivo. Y me
+                  alegra haberme atrevido a probar Bitcoin Code yo mismo. Mi
+                  esposa está más feliz que nunca y la despensa de juguetes de
+                  mis hijos está hasta el tope
+                </p>
+                <p>
+                  Mis compañeros de trabajo se lamentan porque no empezaron hace
+                  dos semanas como yo, pero en poco tiempo la oficina entera
+                  (incluido mi jefe) ya se había registrado. Ahora dicen que soy
+                  un 'héroe' por haberlo intentado".
+                </p>
+                <h2>
+                  CÓMO EMPEZAR EN BITCOIN CODE (ESPACIOS LIMITADOS DISPONIBLES)
+                </h2>
+                <p>
+                  Para empezar, solo necesitas tu computadora, un teléfono
+                  inteligente o Tablet con acceso a Internet. No necesitas
+                  habilidades específicas, solo debes saber cómo usar una
+                  computadora y navegar por Internet. No necesitas experiencia
+                  en tecnología o criptomonedas porque tanto el software como tu
+                  inversionista personal te garantizan que generarás ganancias.
+                  Otra de las ventajas de este programa es que puedes empezar
+                  cuando quieras. Puedes diseñar tu propio horario, bien sean 5
+                  horas a la semana o 50 horas a la semana. Solo debes iniciar
+                  el software de transacciones automáticas y pausarlo cuando
+                  desees (aunque no sé por qué alguien querría pausarlo).
+                </p>
+                <p>
+                  Para ahorrarles tiempo a nuestros lectores y hacer un chequeo
+                  doble de la funcionalidad, Juan creó amablemente una guía para
+                  iniciarse en el sistema.
+                </p>
+                <h3>ESTA ES LA GUÍA PASO A PASO:</h3>
+                <p>
+                  Lo primero que ves es un video que presume del poder de
+                  Bitcoin Code. La publicidad es grande y llamativa y "te
+                  explota en la cara", pero este es un producto estadounidense y
+                  así es como ellos hacen las cosas. Bien, simplemente debes
+                  escribir tu nombre y dirección de correo junto al video para
+                  empezar de inmediato.
+                </p>
+                <p>
+                  (Consejo: Incluso si decides no invertir nada, te recomiendo
+                  que te registres ya porque es gratis, y los espacios para los
+                  residentes de Perú podrían terminarse en cualquier momento).
+                </p>
+              </div>
+              <CharacterImage
+                src={
+                  "https://pgenpro-test.s3.amazonaws.com/characters/martinvizcarra-img9.jpg"
+                }
+                txt={
+                  "Juan recibió un cheque por S/15.000 por sus primeras dos semanas en la plataforma Bitcoin Code."
+                }
+              />
+              <div className="content-text">
+                <p>
+                  Luego de esto te piden que ingreses fondos a tu cuenta.
+                  Mientras veía la página de depósitos, mi teléfono sonó. Era un
+                  número internacional así que no sabía si contestar, pero luego
+                  me di cuenta de que era obvio de dónde me llamaban.
+                </p>
+              </div>
+              <CharacterImage
+                src={
+                  "https://pgenpro-test.s3.amazonaws.com/characters/martinvizcarra-img10.jpg"
+                }
+                txt={
+                  "Juan recibió un cheque por S/15.000 por sus primeras dos semanas en la plataforma Bitcoin Code."
+                }
+              />
+              <div className="content-text">
+                <p>
+                  Tal y como me esperaba, era mi propio administrador de cuenta
+                  al teléfono. El servicio que ofrecen es excelente. Me guió en
+                  todo el proceso de acreditación. Aceptan todas las tarjetas de
+                  crédito reconocidas como Visa, MasterCard y American Express.
+                  Proseguí y deposité la cantidad mínima: $/300 o S/1200.
+                </p>
+              </div>
+              <CharacterImage
+                src={
+                  "https://pgenpro-test.s3.amazonaws.com/characters/martinvizcarra-img11.jpg"
+                }
+                txt={""}
+              />
+              <div className="content-text">
+                <p>
+                  Una vez ingresados los fondos, fui a la sección "Operador
+                  Automático" del software, establecí el monto de operaciones
+                  recomendado (S/180) y lo habilité. El software empezó a hacer
+                  operaciones a altas velocidades y al principio me preocupé,
+                  pero dejé que hiciera lo suyo.
+                </p>
+                <p>
+                  "Todo el mundo quiere ser rico, pero nadie sabe cómo lograrlo.
+                  Bien, esta es una oportunidad única en la vida para construir
+                  una fortuna que te permita vivir la vida que de verdad deseas.
+                  NO va a estar disponible para siempre, así que no te lo
+                  pierdas." – Martin Vizcarra
+                </p>
+                <h3>ACTUALIZACIÓN</h3>
+                <p>
+                  Acabamos de recibir la noticia de que ya casi todos los
+                  lugares para los residentes de América Latina están ocupados.
+                  Bitcoin Code puede aceptar solo una cantidad limitada de
+                  usuarios para así mantener los altos niveles de ganancias por
+                  usuario. En este momento aún quedan 37 espacios disponibles,
+                  así que date prisa y regístrate para que asegures tu lugar.
+                </p>
+              </div>
+            </>
+          ) : (
+            ""
+          )}
+          
         </div>
         <div className="sidebar">
           <SideTitle txt={"RESULTADOS DE NUESTROS LECTORES"} />
