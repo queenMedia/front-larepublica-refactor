65a51f175f87d6b794bdc5c72b954cd289915a3c
ronyderra <derrarony@gmail.com>
2023-06-12 14:12:21 +0300

    comments

--- src/sections/comments/comments.js
+++ src/sections/comments/comments.js
@@ -17,6 +17,182 @@ export const comments = [
     number: "6 . Me gusta",
     time: "Hace 13 minutos",
   },
+  {
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/ashley.jpg",
+    name: "Jenifer García",
+    description:
+      "Vi Bitcoin Code en el proyecto y me registré ayer, estoy ganando USD/89.",
+    number: "19 . Me gusta",
+    time: "Hace 13 minutos",
+  },
+  {
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/alice.jpg",
+    name: "Miguel Duarte",
+    description:
+      "Vi Bitcoin Code en el proyecto y me registré ayer, estoy ganando USD/89.",
+    number: "1 . Me gusta",
+    time: "Hace 13 minutos",
+  },
+  {
+    name: "David Barragán",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/cash.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "43 . Me gusta",
+    time: "Hace 1 hora",
+  },
+  {
+    name: "Amanda Huerta",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/kirs.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "3 . Me gusta",
+    time: "Hace 1 hora",
+  },
+  {
+    name: "Julia Sueños",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/got.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "1 . Me gusta",
+    time: "Hace 2 horas",
+  },
+  {
+    name: "Pedro Sastre",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/jill.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "12 . Me gusta",
+    time: "Hace 2 horas",
+  },
+  {
+    name: "Cristina Mantaruelo",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/jenni.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Celia Santos",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/jenna.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Elena López",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/hick.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Alex Casas",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/mark.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Marcos Falemai",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/katy.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Ana Rota",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/kirs.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Esther Calatrava",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/julie.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Jonathan Vázquez",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/laura.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Juan García",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/silver.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Pablo Garzón",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/sara.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "María Cuevas",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/amanda.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Gloria Ramírez",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/tanya.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Rubén Fargas",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/shel.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Tomás Prieto",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/sarah.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Ernesto Cádiz",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/cash.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
+  {
+    name: "Gregoria Guardado",
+    image: "https://pgenpro-test.s3.amazonaws.com/Comments/molly.jpg",
+    description:
+      "Las últimas semanas he estado operando con Bitcoin Code y he ganado USD/8,238. ¡Me encanta!",
+    number: "13 . Me gusta",
+    time: "Hace 12 minutos",
+  },
 ];
 
 export const Comment = (props) => {
