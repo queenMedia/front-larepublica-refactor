949d40bc4f1dd340d7a8012f45a8e03222e5a607
ronyderra <derrarony@gmail.com>
2023-06-12 16:38:22 +0300

    links

--- src/sections/sideBar/sideBar.js
+++ src/sections/sideBar/sideBar.js
@@ -2,6 +2,7 @@ import "./sideBar.css";
 import { SideTitle } from "../../components/sideTitle/sideTitle";
 import { SideTestimonial } from "../../components/sideTestimonial/sideTestimonial";
 import { Step1 } from "../../components/steps/step1";
+import { LINK } from "../../config";
 const SideBar = () => {
   return (
     <>
@@ -10,7 +11,9 @@ const SideBar = () => {
       <SideTitle txt={"GANANCIAS: S/26,952"} />
       <SideTestimonial
         txt={
-          "He estado usando Bitcoin Code durante apenas 2 semanas y ya pagó mis futuras vacaciones por México."
+          <p>
+            "He estado usando <a href={LINK} target="_blank">Bitcoin Code</a> durante apenas 2 semanas y ya pagó mis futuras vacaciones por México."
+          </p>
         }
         name={"Sofia Hernández Callao"}
         src={"https://pgenpro-test.s3.amazonaws.com/fakeNames/victoriaPE.png"}
@@ -19,7 +22,7 @@ const SideBar = () => {
       <SideTitle txt={"GANANCIAS: S/79,620"} />
       <SideTestimonial
         txt={
-          "¡Es tan fácil de usar, incluso para mí! No había hecho transacciones antes, pero ahora hago más de S/10,000 a la semana, ¡y amo mi vida!"
+          <p>"¡Es tan fácil de usar, incluso para mí! No había hecho transacciones antes, pero ahora hago más de S/10,000 a la semana, ¡y amo mi vida!" </p>
         }
         name={"Jimena Alba Chiclayo"}
         src={"https://pgenpro-test.s3.amazonaws.com/fakeNames/jimenaPE.png"}
@@ -27,8 +30,10 @@ const SideBar = () => {
       {/* number 3 */}
       <SideTitle txt={"GANANCIAS: S/79,620"} />
       <SideTestimonial
-        txt={
-          "Ya hice S/33,000 en ganancias después de usar Bitcoin Code durante apenas un mes. ¡Ya que puedo usarlo en mi laptop, he estado viajando por todo Perú mientras hago dinero!"
+         txt={
+          <p>
+            "Ya hice S/33,000 en ganancias después de usar <a href={LINK} target="_blank">Bitcoin Code</a> durante apenas un mes. ¡Ya que puedo usarlo en mi laptop, he estado viajando por todo Perú mientras hago dinero!"
+          </p>
         }
         name={"Alejandro Chávez Piura"}
         src={"https://pgenpro-test.s3.amazonaws.com/fakeNames/alejandroPE.png"}
@@ -37,7 +42,7 @@ const SideBar = () => {
       <SideTitle txt={"GANANCIAS: S/33,281"} />
       <SideTestimonial
         txt={
-          "Mi novio fue quien me contó de esto, me cambió la vida. He hecho más de S/7,000 a la semana durante más de un mes, con menos de 30 minutos de trabajo al día."
+          <p>"Mi novio fue quien me contó de esto, me cambió la vida. He hecho más de S/7,000 a la semana durante más de un mes, con menos de 30 minutos de trabajo al día."</p>
         }
         name={"Victoria Pérez Huancayo"}
         src={"https://pgenpro-test.s3.amazonaws.com/fakeNames/sofiaPE.png"}
@@ -46,7 +51,7 @@ const SideBar = () => {
       <SideTitle txt={"GANANCIAS: S/44,080"} />
       <SideTestimonial
         txt={
-          "Me uní con mis mejores amigos y juntos hemos ganado muchísimo después de tan solo 3 semanas. El robot de las transacciones hace todo el trabajo por ti. Juntos hemos hecho más de S/44860 a la semana."
+          <p> "Me uní con mis mejores amigos y juntos hemos ganado muchísimo después de tan solo 3 semanas. El robot de las transacciones hace todo el trabajo por ti. Juntos hemos hecho más de S/44860 a la semana."</p>
         }
         name={"Alejandro Flores & Carlo Lima Arequipa"}
         src={"https://pgenpro-test.s3.amazonaws.com/fakeNames/alecarlopng.png"}
@@ -54,18 +59,22 @@ const SideBar = () => {
       {/* number 6 */}
       <SideTitle txt={"GANANCIAS: S/210,529"} />
       <SideTestimonial
-        txt={
-          "Al fin pude renunciar a mi trabajo, todo gracias a Bitcoin Code. ¡He hecho tanto dinero, tan fácil!"
-        }
+          txt={
+            <p>
+              "Al fin pude renunciar a mi trabajo, todo gracias a <a href={LINK} target="_blank">Bitcoin Code</a>. ¡He hecho tanto dinero, tan fácil!"
+            </p>
+          }
         name={"Mateo Guerra Trujillo"}
         src={"https://pgenpro-test.s3.amazonaws.com/fakeNames/mateoPE.png"}
       />
       {/* number 7 */}
       <SideTitle txt={"GANANCIAS: S/150,300"} />
       <SideTestimonial
-        txt={
-          "Me registré en Bitcoin Code hace un poco más de dos semanas. Convertí mi depósito inicial de S/1200 en S/20,787. Esto es mucho más de lo que gano en mi trabajo."
-        }
+     txt={
+      <p>
+        "Me registré en <a href={LINK} target="_blank">Bitcoin Code</a> hace un poco más de dos semanas. Convertí mi depósito inicial de S/1200 en S/20,787. Esto es mucho más de lo que gano en mi trabajo."
+      </p>
+    }
         name={"Santiago Sánchez Lima"}
         src={"https://pgenpro-test.s3.amazonaws.com/fakeNames/santiagoPE.png"}
       />
