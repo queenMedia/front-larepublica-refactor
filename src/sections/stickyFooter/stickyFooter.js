1636a7e0e841ea65064d2c1118f234edce92915e
ronyderra <derrarony@gmail.com>
2023-06-12 12:25:28 +0300

    stickyfooter

---
+++ src/sections/stickyFooter/stickyFooter.js
@@ -0,0 +1,11 @@
+import "./stickyFooter.css";
+
+const StickyFooter = () => {
+  return (
+    <div className="stickyFooter">
+      <button>PRUEBA Bitcoin Code POR TI MISMO</button>
+    </div>
+  );
+};
+
+export default StickyFooter;
