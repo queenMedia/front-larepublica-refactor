f65046edf3c55117f210af81d56ec7cee37afa82
ronyderra <derrarony@gmail.com>
2023-06-13 12:27:09 +0300

    subtitle

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export const LINK = "https://givemes.com/outgoing";
 export const CHARACTER = "Martin Vizcarra"
+export const SUBTITLE_FIRST_LINE = "Ya los latinoamericanos están amasando millones de USD desde sus casas gracias a"
+export const SUBTITLE_SECOND_LINE = `esta "laguna", pero ¿es legal?`
 
 export const handleRoute = () => {
   window.open(LINK, "_blank", "noopener,noreferrer");
