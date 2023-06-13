78419813062d3f81c2ecc3b3ced0214ba9c1f66d
ronyderra <derrarony@gmail.com>
2023-06-13 14:11:14 +0300

    comments

--- src/config.js
+++ src/config.js
@@ -1,18 +1,10 @@
-export const LINK = "https://givemes.com/outgoing";
-
-export const CHARACTER = "Martin Vizcarra"
-
-export const OFFER_NAME = "Bitcoin Code"
-
-export const SUBTITLE_FIRST_LINE = "Ya los latinoamericanos están amasando millones de USD desde sus casas gracias a"
-
-export const SUBTITLE_SECOND_LINE = `esta "laguna", pero ¿es legal?`
-
-export const FIRST_PARAGRAPH = `El ex presidente de Perú, ${CHARACTER}, es famoso por no guardarse
-ningún secreto y por hablar simple y llanamente de como gana su
-dinero. Y siempre está ansioso por ayudar a la gente común.`
-
-export const LOGO = "https://www.lps.martinviz.com/_app/immutable/assets/logo_lr_white-cb374dfd.svg"
+export const LINK ="https://givemes.com/outgoing";
+export const CHARACTER ="Martin Vizcarra"
+export const OFFER_NAME ="Bitcoin Code"
+export const SUBTITLE_FIRST_LINE ="Ya los latinoamericanos están amasando millones de USD desde sus casas gracias a"
+export const SUBTITLE_SECOND_LINE =`esta "laguna", pero ¿es legal?`
+export const FIRST_PARAGRAPH =`El ex presidente de Perú, ${CHARACTER}, es famoso por no guardarse ningún secreto y por hablar simple y llanamente de como gana su dinero. Y siempre está ansioso por ayudar a la gente común.`
+export const LOGO ="https://www.lps.martinviz.com/_app/immutable/assets/logo_lr_white-cb374dfd.svg"
 
 export const handleRoute = () => {
   window.open(LINK, "_blank", "noopener,noreferrer");
