7050a9ebf9e3fb0fcdd82d0eafc678fdd9adf7aa
sergiorgomar <sergio@callytek.com>
2023-06-23 12:47:59 -0600

    FIX: offer name

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import MODAL_IMG from "./assets/images/modal/BITCOIN360AI.jpeg";
 
 export const LINK = "https://givemes.com/outgoing";
 export const CHARACTER = "Dina Boluarte";
-export const OFFER_NAME = "Bitcoin360AI";
+export const OFFER_NAME = "BITCOIN 360Ai";
 export const MODAL = MODAL_IMG;
 export const SUBTITLE_FIRST_LINE ="Ya los latinoamericanos están amasando millones de USD desde sus casas gracias a";
 export const SUBTITLE_SECOND_LINE = `esta "laguna", pero ¿es legal?`;
