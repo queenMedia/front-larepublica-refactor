36968ef0356adb2ffd6339e743b3ab7d285a0a31
sergiorgomar <sergio@callytek.com>
2023-06-22 18:13:02 -0600

    FEAT: quantumai offer added

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import MODAL_IMG from "./assets/images/modal/MODAL.png";
 
 export const LINK = "https://givemes.com/outgoing";
 export const CHARACTER = "Dina Boluarte";
-export const OFFER_NAME = "BitSoft360";
+export const OFFER_NAME = "QuantumAI";
 export const MODAL = MODAL_IMG;
 export const SUBTITLE_FIRST_LINE ="Ya los latinoamericanos están amasando millones de USD desde sus casas gracias a";
 export const SUBTITLE_SECOND_LINE = `esta "laguna", pero ¿es legal?`;
